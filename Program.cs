0bebf57272091c14cf1b1f6388c1e1e959133669
habib24457 <habiburehman390@gmail.com>
2023-11-06 08:13:47 +0100

    Added Database and DI

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoContext>
+(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("localDBConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
